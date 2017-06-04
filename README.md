690811d3f078569c44f0032a7e30271a68b99a0e
terminalcommand <outdoor@localhost.localdomain>
2017-06-04 18:46:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# FolderSize
+Automatically appends the size to your folder's name.
+
+e.g. SomeFolder (1.20 GiB)
+
+# How to Install
+Just grab Python from http://python.org, Python 3 specifically
+Then run python3 main.py, in the folder you want to rename.
+Further instructions for easy-use (e.g. right-click) will follow.
+
+# Why did you write this
+A friend of mine had the idea, I thought it was cool.
+Mike, if you're reading this contribute! :)
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import logging
+from os.path import join, getsize
+import re
+
+def bytes2human(size):
+    gibi = 1024*1024*1024
+    mibi = 1024*1024
+    kibi = 1024
+    if size >= gibi:
+        return str(round(size/gibi, 2))+" GiB"
+    elif size >= mibi:
+        return str(round(size/mibi, 2))+" MiB"
+    elif size >= kibi:
+        return str(round(size/kibi, 2))+" KiB"
+    else:
+        return str(size)+" Bytes"
+
+def clearsize(foldername):
+    match = re.search("\(\d+(\.\d+)?.(Bytes|GiB|MiB|KiB)\)",
+                      foldername)
+    if match:
+        stripped = \
+        foldername[:match.start()] + foldername[match.end():]
+#        print("Stripped: "+stripped)
+        return stripped
+    else:
+        return foldername
+
+
+
+for root, dirs, files in os.walk(os.curdir, topdown=True):
+    rootname = os.getcwd()
+    size = 0
+    for name in files:
+        filepath = join(root, name)
+        size += getsize(filepath)
+        
+    try:
+        desc = '('+bytes2human(size)+')'
+        cleared = clearsize(rootname)
+#        print("Rootname: "+ rootname)
+#        print("Cleared: "+cleared)
+        os.rename(rootname, cleared+desc)
+        print("Successfully renamed dir to", cleared+desc)
+            
+    except Exception as err:
+        logging.error(err)
